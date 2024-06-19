9917ea7359c103faa5f313e5428d9a988b616c1e
Jake Bulman <jakebulman@hotmail.com>
2024-06-19 18:19:14 +0100

    Install and configure postgres database

--- castandcrew/castandcrew/settings.py
+++ castandcrew/castandcrew/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv('..env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +78,14 @@ WSGI_APPLICATION = 'castandcrew.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'castandcrew',
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'USER': 'castandcrew',
+        'PASSWORD': 'jman',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 

1c27a17794cd6089b0e88446f9abbd6331ae5e8d
Jake Bulman <jakebulman@hotmail.com>
2024-06-21 23:35:51 +0100

    Undo test debug

--- castandcrew/castandcrew/settings.py
+++ castandcrew/castandcrew/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY'),
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost','13.51.169.173','thecastandcrew.co.uk','www.thecastandcrew.co.uk']
 

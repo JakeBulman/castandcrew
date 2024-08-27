1b2c969d6e733aee63704dfaec3c0710b1c41167
Jake Bulman <jakebulman@hotmail.com>
2024-08-27 13:35:07 +0100

    no env variables test

--- castandcrew/castandcrew/settings.py
+++ castandcrew/castandcrew/settings.py
@@ -21,14 +21,14 @@ import boto3
 load_dotenv(find_dotenv())
 
 #Load all env variables from secrets manager
-secret_name = os.getenv('SECRET_LOCATION')
-region_name = 'eu-north-1'
-session = boto3.session.Session(aws_access_key_id=os.getenv('AWS_SERVER_PUBLIC_KEY'),aws_secret_access_key=os.getenv('AWS_SERVER_SECRET_KEY'))
-client = session.client(
-    service_name='secretsmanager',
-    region_name=region_name
-)
-secrets = client.get_secret_value(SecretId=secret_name)
+# secret_name = os.getenv('SECRET_LOCATION')
+# region_name = 'eu-north-1'
+# session = boto3.session.Session(aws_access_key_id=os.getenv('AWS_SERVER_PUBLIC_KEY'),aws_secret_access_key=os.getenv('AWS_SERVER_SECRET_KEY'))
+# client = session.client(
+#     service_name='secretsmanager',
+#     region_name=region_name
+# )
+# secrets = client.get_secret_value(SecretId=secret_name)
 
 
 
@@ -40,12 +40,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = json.loads(secrets['SecretString'])['SECRET_KEY'],
+# SECRET_KEY = json.loads(secrets['SecretString'])['SECRET_KEY'],
+SECRET_KEY = 'django-insecure--52g3t0u4&u2bvfd(ir7(lhm4yvg_13aj37$jnyl025f$8$(pe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','13.51.169.173','thecastandcrew.co.uk','www.thecastandcrew.co.uk','lokey.co.uk','www.lokey.co.uk']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','13.51.169.173','lokey.co.uk','www.lokey.co.uk']
 
 
 # Application definition
@@ -61,7 +62,7 @@ INSTALLED_APPS = [
 
     #other apps
     'compressor',
-    'storages'
+    # 'storages'
 ]
 
 MIDDLEWARE = [
@@ -110,16 +111,16 @@ WSGI_APPLICATION = 'castandcrew.wsgi.application'
 # }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': json.loads(secrets['SecretString'])['DB_NAME'],
-        'USER': json.loads(secrets['SecretString'])['POSTGRES_USER'],
-        'PASSWORD': json.loads(secrets['SecretString'])['DB_PASSWORD'],
-        'HOST': json.loads(secrets['SecretString'])['DB_HOST'],
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': json.loads(secrets['SecretString'])['DB_NAME'],
+#         'USER': json.loads(secrets['SecretString'])['POSTGRES_USER'],
+#         'PASSWORD': json.loads(secrets['SecretString'])['DB_PASSWORD'],
+#         'HOST': json.loads(secrets['SecretString'])['DB_HOST'],
+#         'PORT': '5432',
+#     }
+# }
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
@@ -136,29 +137,29 @@ COMPRESS_PRECOMPILERS = (
     ('text/x-scss', 'django_libsass.SassCompiler'),
 )
 
-USING_S3 = json.loads(secrets['SecretString'])['USING_S3'] == 'True'
+# USING_S3 = json.loads(secrets['SecretString'])['USING_S3'] == 'True'
 
-if USING_S3:
-    print('S3 is on')
-    AWS_ACCESS_KEY_ID = json.loads(secrets['SecretString'])['AWS_ACCESS_KEY_ID']
-    AWS_SECRET_ACCESS_KEY = json.loads(secrets['SecretString'])['AWS_SECRET_ACCESS_KEY']
-    AWS_STORAGE_BUCKET_NAME = json.loads(secrets['SecretString'])['AWS_STORAGE_BUCKET_NAME']
-    AWS_S3_SIGNATURE_NAME = json.loads(secrets['SecretString'])['AWS_S3_SIGNATURE_NAME']
-    AWS_S3_REGION_NAME = json.loads(secrets['SecretString'])['AWS_S3_REGION_NAME']
-    AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-    AWS_S3_VERITY = True
-    AWS_LOCATION = 'static'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATIC_ROOT = 'storages.backends.s3boto3.S3Boto3Storage'
-else:
-    STATIC_URL = 'static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# if USING_S3:
+#     print('S3 is on')
+#     AWS_ACCESS_KEY_ID = json.loads(secrets['SecretString'])['AWS_ACCESS_KEY_ID']
+#     AWS_SECRET_ACCESS_KEY = json.loads(secrets['SecretString'])['AWS_SECRET_ACCESS_KEY']
+#     AWS_STORAGE_BUCKET_NAME = json.loads(secrets['SecretString'])['AWS_STORAGE_BUCKET_NAME']
+#     AWS_S3_SIGNATURE_NAME = json.loads(secrets['SecretString'])['AWS_S3_SIGNATURE_NAME']
+#     AWS_S3_REGION_NAME = json.loads(secrets['SecretString'])['AWS_S3_REGION_NAME']
+#     AWS_S3_FILE_OVERWRITE = False
+#     AWS_DEFAULT_ACL = 'public-read'
+#     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+#     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+#     AWS_S3_VERITY = True
+#     AWS_LOCATION = 'static'
+#     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+#     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#     STATIC_ROOT = 'storages.backends.s3boto3.S3Boto3Storage'
+# else:
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators

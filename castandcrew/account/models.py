fbccae04bd6d96c440a12c18687d104c3935336e
Jake Bulman <jakebulman@hotmail.com>
2024-09-04 18:01:39 +0100

    Account update messages and usable filepaths

--- castandcrew/account/models.py
+++ castandcrew/account/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.conf import settings
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return "users/{0}/{1}".format(instance.user.id, filename)
+
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     stage_name = models.CharField(max_length=100, blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
     website_link = models.TextField(blank=True, null=True)
     user_about = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+    profile_picture = models.ImageField(upload_to=user_directory_path, blank=True)
     #portfolio pictures as a seperate model? (this allows for title and description)
     #portfolio audio as a seperate model? (this allows for title and description)
     #portfolio video as a seperate model? (this allows for title and description)
-    
+
--- castandcrew/account/views.py
+++ castandcrew/account/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
+from django.contrib import messages
 
 @login_required
 def dashboard(request):
@@ -37,6 +38,9 @@ def edit(request):
 		if user_form.is_valid() and profile_form.is_valid():
 			user_form.save()
 			profile_form.save()
+			messages.success(request, 'Profile updated successfully')
+		else:
+			messages.error(request, 'Error updating your profile')
 	else:
 		user_form = UserEditForm(instance=request.user,prefix="user")
 		profile_form = ProfileEditForm(instance=request.user.profile,prefix="profile")
